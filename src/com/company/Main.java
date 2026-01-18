300b70641bc026394fe844638edfe6a73ef1c995
dilnazzhamantayeva <dicosiya07@gmail.com>
2026-01-18 14:08:11 +0500

    WIP: save Main before merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         // Here you specify which DB and UserRepository to use
         // And changing DB should not affect to whole code
-        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "*Asem184)8", "somedb");
+        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "0000", "somedb");
         IUserRepository repo = new UserRepository(db);
         IUserController controller = new UserController(repo);
 
